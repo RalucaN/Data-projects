0c5e495805e6a9c9335a74317c79744186d4da18
Raluca Nicoara <nicoarar@tcd.ie>
2020-03-08 21:39:33 +0000

    update

--- Women_in_Data_Science_programme_(Feb 2020-)/multinomial regression/multinomial_regression.R
+++ Women_in_Data_Science_programme_(Feb 2020-)/multinomial regression/multinomial_regression.R
@@ -8,26 +8,8 @@ Data_processed$Result_Type <- relevel(Data_processed$Result_Type, ref = "PASS")
 
 model<-multinom(Result_Type ~ Block_Position+Block_Num+Zone3_Humidity_Avg+Zone2_Humidity_Avg+
                   Zone1_Humidity_Avg+Zone3_Temp_Avg+Zone2_Temp_Avg+Zone1_Temp_Avg+Zone2_Dur+
-                  Zone1_Dur+Zone3_Dur+Zone1Position+Zone2Position+Zone2Position+SKU+
-                  Zone1_Out_Zone2_In_Dur+Zone2_In_Zone3_Out_Dur+Zone1_In_Zone2_Out_Dur+
-                  Zone1_Out_Zone3_In_Dur+Zone2_Out_Zone3_In_Dur+Zone1_In_Zone3_Out_Dur, 
-                data = Data_processed)
-
-z <- summary(model)$coefficients/summary(model)$standard.errors
-
-pvalue <- (1 - pnorm(abs(z), 0, 1)) * 2
-
-
-model<-multinom(Result_Type ~ Block_Position+Block_Num+Zone3_Humidity_Avg+Zone2_Humidity_Avg+
-                  Zone1_Humidity_Avg+Zone3_Temp_Avg+Zone2_Temp_Avg+Zone1_Temp_Avg+Zone2_Dur+
                   Zone1_Dur+Zone3_Dur+Zone1Position+Zone2Position+Zone3Position+SKU, 
                 data = Data_processed)
-
-z <- summary(model)$coefficients/summary(model)$standard.errors
-
-pvalue <- (1 - pnorm(abs(z), 0, 1)) * 2
-
-stargazer(model, type = "html", style = "apsr", out = "model1.html")
 
 model2<-multinom(Result_Type ~ Block_Position+Block_Num+Zone3_Humidity_Min+Zone2_Humidity_Min+
                   Zone1_Humidity_Min+Zone3_Temp_Min+Zone2_Temp_Min+Zone1_Temp_Min+Zone2_Dur+
@@ -71,10 +53,11 @@ model8<-multinom(Result_Type ~ Block_Position+Block_Num+Zone3_Humidity_Avg+Zone2
                  data = Data_processed)
 
 stargazer(model, model2,model3,model4, model5, model6, type = "html", style = "apsr", 
-          out = "model2.html")
+          out = "model1_6.html")
 
-stargazer(model4, type = "html", style = "apsr", 
-          out = "model4.html")
+#model4 has the best fit due to a lower AIC
+
+stargazer(model4, type = "html", style = "apsr", out = "model4.html")
 
 stargazer(model7, model8, type = "html", style = "apsr", 
           out = "model7_8.html")
